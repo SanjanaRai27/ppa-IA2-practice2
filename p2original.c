24dc880a1ea2d2897f4920918f09c313eda3dc1b
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-02-09 11:25:10 +0000

    p2done

--- p2original.c
+++ p2original.c
@@ -1,30 +1,33 @@
 #include<stdio.h>
-#include<math.h>
 int input_side()
 {
-int a;
-printf("Enter the sides of triangle:\n");
-scanf("%d",&a);
-return a;
+  int x;
+  printf("Enter the side length:\n");
+  scanf("%d",&x);
+  return x;
 }
-int check_scalene(int a,int b,int c)
+int check_scalene(int a,int b,int c,int *isscalene)
 {
-int s,area;
-s=(a+b+c)/2;
-area=sqrt(s*(s-a)(s-b)(s-c));
-return area;
+  if((a!=b) && (b!=c) && (a!=c))
+  *isscalene=1;
+  else 
+  *isscalene=-1;
 }
-void output(int a,int b,int c,int isscalene)
+void output(int a,int b, int c,int isscalene)
 {
-int area;
-printf("the area of triangle is\n",a,b,c,area);
+  if(isscalene==1)
+  printf("The triangle is a scalene triangle\n");
+  else 
+  if(isscalene==-1)
+  printf("The triangle is not a scalene triangle\n");
 }
 int main()
 {
-int p,q,r,k;
-p=input_side();
-q=input_side();
-r=input_side();
-output(p,q,r,k);
-return 0;
+  int a,b,c,isscalene;
+  a=input_side();
+  b=input_side();
+  c=input_side();
+  check_scalene(a,b,c,&isscalene);
+  output(a,b,c,isscalene);
+  return 0;
 }
