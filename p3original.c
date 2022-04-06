8f98b237898f14d04ff035b9c18b20bb3e1b6124
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-04-06 14:13:47 +0000

    p5done

--- p3original.c
+++ p3original.c
@@ -1,46 +1,34 @@
 #include<stdio.h>
 int input_number()
 {
-int a;
-printf("Enter a number:\n");
-scanf("%d",&a);
-return a;
+  int n;
+  printf("Enter a number: \n");
+  scanf("%d",&n);
+  return n;
 }
 int is_composite(int n)
 {
-int a;
-int flag=0;
-if(a<=2){
-  printf("Enter the number greater than 2");
+  int i, count =0;
+  for(i=1; i<=n; i++)
+   {
+    if(n % i == 0)
+    count++;
+   }
+return count;  
 }
-else{
-  for(int i=2;i<a;i++){
-    if(a%i==0){
-      printf("its a composite number");
-      flag=1;
-      break;
-    }
-  }
-}
-if (flag==0){
- return(0);
-}
-return(1) ;
-}
-void output(int n, int composite)
+void output(int n,int composite)
 {
-if(composite==0)
-{
-  printf("it is not composite");
-}else{
-  printf("it is composite");
-}
-
+  int result;
+  if(composite > 2)
+    printf("The number %d is composite",n);
+  else
+    printf("The number is a prime number");
 }
 int main()
 {
-  int p,q;
-  p=input_number();
-  output(q,p);
+  int n,composite;
+  n=input_number();
+  composite=is_composite(n);
+  output(n,composite);
   return 0;
 }
--- p5original.c
+++ p5original.c
@@ -1,36 +1,31 @@
-
-include<stdio.h>
+#include<stdio.h>
 int input()
 {
-int a;
-printf("Enter the value of two numbers:\n");
-scanf("%d",&a);
-return a;
+  int x;
+  printf("Enter the numbers\n");
+  scanf("%d",&x);
+  return x;
 }
 int gcd(int a,int b)
 {
- int min,gcd;
- min=(a<b)?a:b;
- for(int i=1;i<=min;i++)
- {
-   if(a%i==0 && b%i==0)       
-   {
-     gcd=i;
-     printf("%d",i);
-   }
- }
-return gcd;
+  int g;
+  for(int i=1; i<=a && i<=b;i++)
+    {
+      if(a%i==0 && b%i==0)
+        g=i;
+    }
+  return g;
 }
-void output(int a,int b,int gcd)
-{
-printf("The gcd of %d and %d is %d\n",a,b,gcd);
-}
-int main()
-{
-int p,q,r;
-p=input();
-q=input();
-r=gcd(p,q);
-output(p,q,r);
-return 0;
-}
+  void output(int a,int b,int gcd)
+  {
+    printf("The gcd of %d and %d is %d",a,b,gcd);
+  }
+  int main()
+  {
+    int a,b,c;
+    a=input();
+    b=input();
+    c=gcd(a,b);
+    output(a,b,c);
+    return 0;
+  } 
